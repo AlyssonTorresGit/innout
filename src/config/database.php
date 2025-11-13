9ca40e2c2c78eab22155347b8ee13a053ca03e08
AlyssonTorresGit <alysson-b@hotmail.com>
2025-11-13 14:03:08 -0300

    gsdfgf

---
+++ src/config/database.php
@@ -0,0 +1,43 @@
+<?php
+
+use LDAP\Result;
+
+class Produto
+{
+    public $nome;
+    public $preco;
+}
+
+$produto1 = new Produto();
+print_r($produto1);
+echo '<br>';
+$produto1->nome = 'alysson';
+$produto1->preco = '31';
+
+class Database
+{
+    public static function getConnection()
+    {
+        $envPath = realpath(dirname(__FILE__) . '/../env.ini');
+        $env = parse_ini_file($envPath); // transforma o $env em um array com od dados do banco (env.ini), Array ( [host] => localhost [username] => root [password] => [database] => innout )
+        $conn = new mysqli($env['host'], $env['username'], $env['password'], $env['database'],);
+
+        print_r($env);
+        echo '<br>';
+
+        if ($conn->connect_error) {
+            die("Erro: " . $conn->connect_error);
+        }
+
+        return $conn;
+    }
+    public static function getResultFromQuery($sql)
+    {
+        $conn = self::getConnection();
+        $result = $conn->query($sql);
+        print_r($result);
+        echo '<br>';
+        $conn->close();
+        return ($result);
+    }
+}
